5087da67fc2fd773b91b0d4fb8491d371cb6d54f
Benjamin <76777387+bsoo3@users.noreply.github.com>
2021-09-24 16:14:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function countdown() {
-    document.getElementById("countdown")
+const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
+const quoteDisplayElement = document.getElementById('quoteDisplay')
+const quoteInputElement = document.getElementById('quoteInput')
+const timerElement = document.getElementById('timer')
+
+function getRandomQuote() {
+    return fetch(RANDOM_QUOTE_API_URL)
+        .then(response => response.json)
+        .then(data => data.content)
 }
+
+async function renderNewQuote() {
+    const quote = awit getRandomQuote()
+    quoteDisplayElement.innerText = quote
+}
+
+renderNewQuote()
