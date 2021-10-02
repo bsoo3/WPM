18bc8f97bea11fccf9a7e4917bf2ef5b67622c8a
Benjamin <76777387+bsoo3@users.noreply.github.com>
2021-10-02 22:27:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,7 @@
-/*
-
-const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
-const quoteDisplayElement = document.getElementById('quoteDisplay')
-const quoteInputElement = document.getElementById('quoteInput')
-const timerElement = document.getElementById('timer')
-
-//every time you type it detects it
-quoteInputElement.addEventListener('input', () => {
-    const arrayQuote = quoteDisplayElement.querySelectorAll('span')
-    const arrayValue = quoteInputElement.value.split('')
-    let correct = true
-
-    arrayQuote.forEach((characterSpan, index) => {
-
-        const character = arrayValue[index]
-
-        if (character == null) {
-            characterSpan.classList.remove('correct')
-            characterSpan.classList.remove('incorrect')
-            correct = false
-
-        } else if (character === characterSpan.innerText) {
-            characterSpan.classList.add('correct')
-            characterSpan.classList.remove('incorrect')
-
-        } else {
-            characterSpan.classList.remove('correct')
-            characterSpan.classList.add('incorrect')
-            correct = false
-        }
-    })
-
-    if (correct) renderNewQuote()
-})
-
-
-function getRandomQuote() {
-    return fetch(RANDOM_QUOTE_API_URL)
-        .then(response => response.json())
-        .then(data => data.content)
-}
-
-async function renderNewQuote() {
-    const quote = await getRandomQuote()
-    quoteDisplayElement.innerHTML = ''
-    quote.split('').forEach(character => {
-        const characterSpan = document.createElement('span')
-        characterSpan.innerText = character
-        quoteDisplayElement.appendChild(characterSpan)
-    })
-    quoteInputElement.value = null
-}
-renderNewQuote() 
-    
-*/
-
-
 const typingDiv = document.getElementById("typing");
 
 
-const text = `The oldest classical` //Greek and Latin writing had little or no space between words and could be written in boustrophedon(alternating directions).
+const text = `The oldest classical Greek and Latin writing had little or no space between words and could be written in boustrophedon(alternating directions).`
 
 
 const characters = text.split('').map(char => {
@@ -100,7 +42,7 @@ const keyListener = document.addEventListener("keydown", ({ key }) => {
         const numberOfWords = text.split(" ").length;
         const wps = numberOfWords / seconds;
         const wpm = wps * 60.0;
-        document.getElementById("wpm").innerText = `wpm = ${parseInt(wpm)}`;
+        document.getElementById("stats").innerText = `wpm = ${parseInt(wpm)}`;
         document.removeEventListener("keydown", keyListener);
         return;
     }
